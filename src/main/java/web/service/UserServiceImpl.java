11bb155022ca2ec57fbed93bc2e31c74580b3dcb
Mazlum2003 <mkakoev@gmail.com>
2024-08-12 10:53:26 +0300

    изменил

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package web.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Transactional;
 import web.Dao.UserDao;
 import web.model.User;
 
@@ -18,29 +19,32 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
+    @Transactional
     @Override
     public List<User> findAll() {
         return userDao.findAll();
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User findById(Long id) {
         return userDao.findById(id);
     }
 
+    @Transactional
     @Override
     public void save(User user) {
         userDao.save(user);
     }
 
+    @Transactional
     @Override
     public void update(User user) {
         userDao.update(user);
     }
-
+    @Transactional
     @Override
     public void delete(Long id) {
         userDao.delete(id);
     }
 }
-
